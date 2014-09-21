bb03517a3074a12c22e28177263fe80a31b26dfe
Rosalin Nayak <rosalin.nayak@insdc01.pwc.com>
2014-09-22 00:56:07 +0530

    R-programming: Asnmt-2: cachematrix
    R programming assignment - Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## set the value of the matrix
+    	inv<- NULL
+    	set <- function(y) 
+	 {
+     	  x <<- y
+     	  inv <<- NULL
+   	 }
+   	## get the value of the matrix
+   	get <- function() x
+   
+   	## set the inverse of the matrix
+   	setinverse <- function(solve) inv <<- solve
+   	getinverse <- function() inv
+   
+   	## get the inverse of the matrix
+   	list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +29,37 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   
+   	## get the inverse of the matrix        
+   	inv <- x$getinverse()
+   
+   	## check if there is the matrix   
+   	if(!is.null(inv)) {
+     	message("getting cached data")
+     	return(inv)
+   	}
+   	## if not: get the inverse of the matrix   
+   	data <- x$get()
+   	inv <- solve(data, ...)
+   	## set the inverse of the matrix 
+   	x$setinverse(inv)
+   	inv
 }
+
+##Test:
+##> x <- rbind(c(4,3),c(3,2))
+##> m <- makeCacheMatrix(x)
+##> m$get()
+##     [,1] [,2]
+##[1,]    4    3
+##[2,]    3    2
+##> cacheSolve(m)
+##     [,1] [,2]
+##[1,]   -2    3
+##[2,]    3   -4
+##> cacheSolve(m)
+##getting cached data
+##     [,1] [,2]
+##[1,]   -2    3
+##[2,]    3   -4
+##> 
